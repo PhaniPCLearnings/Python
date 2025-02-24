c745bcf6f323b15c818bdf77004a0a1cf69aabd1
PhaniPCLearnings <phanipc.learnings@gmail.com>
2025-02-24 13:42:06 +0530

    Add files via upload

---
+++ Day-6/Main.py
@@ -0,0 +1,20 @@
+#Reusable Components we can use - Define and Use the function
+#Normal Function
+def sum_Num(a,b):
+    print(a+b)
+
+sum_Num(1,2)
+
+#Return Keyword in Function
+
+def sum_Add(a,b):
+    return a+b
+
+print(sum_Add(2,3))
+
+
+#we can define function and we need to call it in order to use them
+
+#Just like how we have for loop we have something called while loop which helps us to iterate
+
+#it is our choice to chose which loop is better
