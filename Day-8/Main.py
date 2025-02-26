c75700ce5668ff933f84d00c5a851fde1b0e8501
PhaniPCLearnings <phanipc.learnings@gmail.com>
2025-02-26 13:44:10 +0530

    Add files via upload

---
+++ Day-8/Main.py
@@ -0,0 +1,20 @@
+#Function with Arguements
+def greet(name_User):
+    print("Hey "+name_User)
+    print("How are you doing")
+    print("Hope everything is fine")
+
+
+greet("Phani")
+
+
+
+def details(name,location):
+    print("Name: "+name +" and Location : "+location)
+
+details("Phani","Ongole")
+
+details(location="Ongole",name="Phani")
+
+#whatever order we are providing it will go through that order, we can mention the key name in order to overcome that
+
