9d69ee7e310c236e1552fe052130628f54cadc91
PhaniPCLearnings <phanipc.learnings@gmail.com>
2025-03-06 13:42:55 +0530

    Add files via upload

---
+++ Day-14/GamePlay.py
@@ -0,0 +1,16 @@
+data=[
+    {
+        'name':'A',
+        'Followers Count':4
+
+    },
+    {
+        'name':'B',
+        'Followers Count':7
+
+    },
+    {
+        'name':'C',
+        'Followers Count':13
+    }
+]
