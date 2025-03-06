42a5d2bd177e078e646548330f1529333e516f26
PhaniPCLearnings <phanipc.learnings@gmail.com>
2025-03-06 13:17:45 +0530

    Add files via upload

---
+++ Day-13/Main.py
@@ -0,0 +1,11 @@
+#Debugging is best practice when you're grtting bugs
+
+#Steps:
+"""
+1. Describe the Problem
+2. Reproduce the Bug
+3. Play Computers
+4. Fix Errors
+5. Squash Bugs with Print Statement
+6. Debugger Usage [Install Thonny for better experience]
+"""
