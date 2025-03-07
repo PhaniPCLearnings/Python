c79eedc2e2a650584820695fc791ecff1f9bcb09
PhaniPCLearnings <phanipc.learnings@gmail.com>
2025-03-07 16:16:44 +0530

    Add files via upload

---
+++ Day-15/Coffe_Compositions.py
@@ -0,0 +1,13 @@
+def expresso():
+    print("50 ML Water")
+    print("18g Coffe")
+
+def latte():
+    print("200 ML Water")
+    print("24g Coffe")
+    print("150 ML Milk")
+
+def cappuccino():
+    print("250 ML Water")
+    print("24g Coffe")
+    print("100 ML Milk")
